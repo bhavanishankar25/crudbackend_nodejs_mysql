70c436ac9b2f7a5bfb5bc8093b9b2e092c996d75
bhavanishankar25 <47072061+bhavanishankar25@users.noreply.github.com>
2021-05-11 15:49:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = require("./app/models");
 db.sequelize.sync();
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome" });
 });
 
 
